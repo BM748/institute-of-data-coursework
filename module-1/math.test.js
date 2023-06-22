3a89817af7534fa858b56a5c2c9ea97f5a2614f9
BM7487 <bmm7682@gmail.com>
2023-06-21 22:07:00 -0500

    finish math.test

---
+++ module-1/math.test.js
@@ -0,0 +1,37 @@
+if(add(2,4)!=6){
+    console.error
+}
+
+function add(a,b){
+  return a+b
+}
+
+if(subtract(5,2)!=3){
+    console.error
+}
+
+function subtract(a,b){
+  return a-b
+}
+
+if(multiply(5,5)!=10){
+    console.error
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+if(divide(10,5)!=2){
+    console.error
+}
+
+function divide(a,b){
+    return a/b
+}
+
+
+console.log(add(2,4))
+console.log(subtract(5,2))
+console.log(multiply(5,5))
+console.log(divide(10,5))
