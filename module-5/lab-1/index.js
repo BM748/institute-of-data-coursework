6b63c9ed243b6181615d1861e91cdc4b14d10100
BM7487 <bmm7682@gmail.com>
2023-08-10 19:54:45 -0500

    finished module-5-lab-1

---
+++ module-5/lab-1/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const testRoute = require ('./routes/myTestRoutes');
+const calculatorRoute = require('./routes/calculatorRoute')
+
+const app = express()
+const port = 3000
+const public_dir = 'public';
+
+app.use('/',express.static(public_dir))
+
+app.use('/mytest', testRoute);
+app.use('/calculator', calculatorRoute);
+
+
+app.listen(port, () => {
+console.log(`Example app
+listening at
+http://localhost:${port}`)
+})
---
+++ module-5/lab-1/routes/calculatorRoute.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/add', (req, res) => {
+    const {num1,num2} = req.query;
+
+    const total = parseInt(num1) + parseInt(num2);
+
+    //res.send(`the sum of ${num1} and ${num2} is ${total}`)//:
+    
+
+    res.status(200)
+    res.json({result:total})
+
+});
+router.get('/subtract', (req, res) => {
+    const {num1,num2} = req.query;
+
+    const total = parseInt(num1) - parseInt(num2);
+
+    res.status(200)
+    res.json({result:total})
+
+   
+
+});
+
+router.get('/multiply', (req, res) => {
+    const {num1,num2} = req.query;
+
+    const total = parseInt(num1) * parseInt(num3);
+
+    res.status(200)
+    res.json({result:total})
+
+
+
+});
+
+
+router.get('/divide', (req, res) => {
+    const {num1,num2} = req.query;
+
+    const total = parseInt(num1) / parseInt(num2);
+
+    res.status(200)
+    res.json({result:total})
+
+  
+
+
+});
+
+
+    module.exports = router;
